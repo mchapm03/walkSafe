5561b42c6965555e9b654aebe5ea1c3c28379a3b
Margaret Chapman <margaretchapman@wr-130-64-193-49.medford.tufts.edu>
2016-02-07 17:20:55 -0500

    Added XCode project

---
+++ walkSafe/walkSafe/walkSafe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  walkSafe
+//
+//  Created by Margaret Chapman on 2/7/16.
+//  Copyright © 2016 cs.tufts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
